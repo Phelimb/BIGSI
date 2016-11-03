a233b0286d59b660ba2156ff8c1420f627b5b003
Phelim Bradley <wave@phel.im>
2016-11-03 11:48:51 +0000

    Fixed tests for HLL sketch

--- atlasseq/tests/tests_hll_kmer_sketch.py
+++ atlasseq/tests/tests_hll_kmer_sketch.py
@@ -6,7 +6,7 @@ import hypothesis.strategies as st
 KMER = st.text(min_size=31, max_size=31, alphabet=['A', 'T', 'C', 'G'])
 
 
-@given(kmers=st.lists(KMER, min_size=100, max_size=100, unique=True))
+@given(kmers=st.lists(KMER, min_size=20, max_size=20, unique=True))
 def test_jaccard_index(kmers):
     mc = HyperLogLogJaccardIndex()
     mc.delete_all()
@@ -15,8 +15,8 @@ def test_jaccard_index(kmers):
     assert mc.jaccard_index('1234', '1235') == 1
 
 
-@given(kmers1=st.lists(KMER, min_size=100, max_size=100, unique=True),
-       kmers2=st.lists(KMER, min_size=100, max_size=100, unique=True))
+@given(kmers1=st.lists(KMER, min_size=20, max_size=20, unique=True),
+       kmers2=st.lists(KMER, min_size=20, max_size=20, unique=True))
 def test_jaccard_index2(kmers1, kmers2):
     mc = HyperLogLogJaccardIndex()
     mc.delete_all()
@@ -25,28 +25,15 @@ def test_jaccard_index2(kmers1, kmers2):
     skmers1 = set(kmers1)
     skmers2 = set(kmers2)
     true_sim = float(len(skmers1 & skmers2)) / float(len(skmers1 | skmers2))
-    assert true_sim*.8 <= mc.jaccard_index(
-        '1234', '1235') <= true_sim*1.2
-
-
-@given(kmers1=st.lists(KMER, min_size=100, max_size=100, unique=True),
-       kmers2=st.lists(KMER, min_size=100, max_size=100, unique=True))
-def test_kmer_diff(kmers1, kmers2):
-    mc = HyperLogLogJaccardIndex()
-    mc.delete_all()
-    mc.insert(kmers1, '1234')
-    mc.insert(kmers2, '1235')
-    skmers1 = set(kmers1)
-    skmers2 = set(kmers2)
-    true_diff = float(len(skmers1 ^ skmers2))
-    # true_diff2 = float(len(skmers2 - skmers1))
-    assert true_diff*.8 <= mc.symmetric_difference(
-        '1234', '1235') <= true_diff*1.3
+    true_sdiff = float(len(skmers1 ^ skmers2))
     true_diff = float(len(skmers1 - skmers2))
 
-    assert true_diff*.8 <= mc.difference(
-        '1234', '1235') <= true_diff*1.3
-    true_diff = float(len(skmers2 - skmers1))
-
-    assert true_diff*.8 <= mc.difference(
-        '1235', '1234') <= true_diff*1.3
+    ji = mc.jaccard_index('1234', '1235')
+    sd = mc.symmetric_difference('1234', '1235')
+    dd = mc.difference('1234', '1235')
+    print(ji, true_sim, float(abs(ji-true_sim)))
+    print(sd, true_sdiff, float(abs(sd-true_sdiff)))
+    print(dd, true_diff, float(abs(dd - true_diff)))
+    assert float(abs(ji-true_sim)) <= 0.2
+    assert float(abs(sd-true_sdiff)) <= 5
+    assert float(abs(dd - true_diff)) <= 5
