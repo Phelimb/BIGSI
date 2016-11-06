97b36ac95c60b2d1ace7c76f86e1a65ad8f7fb95
Phelim Bradley <wave@phel.im>
2016-11-06 15:14:44 +0000

    default to 2 redis instance

--- scripts/create_docker_compose.py
+++ scripts/create_docker_compose.py
@@ -2,7 +2,7 @@
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('.'))
 redis_conf = []
-for i in range(20):
+for i in range(2):
     redis = {}
     redis['i'] = i+1
     redis['port'] = 6300+i+1
