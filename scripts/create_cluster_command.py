d6105c422f34534cfe1f94ddc8bd2d0050bd9ea3
Phelim Bradley <wave@phel.im>
2016-11-14 15:25:40 +0000

    create_cluster_command

---
+++ scripts/create_cluster_command.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+import begin
+
+
+@begin.start
+def main(N):
+    N = int(N)
+    ports = [
+        "docker exec atlasseq_redismanager_1 /bin/bash -c 'yes yes | ./redis-trib.rb create --replicas 0"]
+    for i in range(N):
+        port = 6300+i+1
+        port_string = "127.0.0.1:%i" % port
+        ports.append(port_string)
+    print(" ".join(ports)+"'")
