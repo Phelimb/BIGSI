446c4f4504220e3026bcfdbb8e5426a3d9bd0460
Phelim Bradley <wave@phel.im>
2017-02-02 11:37:23 +0000

    Restrict loading of array into batches

--- atlasseq/cmds/merge.py
+++ atlasseq/cmds/merge.py
@@ -18,27 +18,32 @@ process = psutil.Process(os.getpid())
 import time
 
 
-def load_memmap(filename, size):
-    return np.load(filename, mmap_mode='r')
+def load_memmap(filename, rowi, rowj):
+    logger.info("%i MB" % int(process.memory_info().rss/1000000))
+    return np.load(filename, mmap_mode='r')[rowi:rowj, :]
     # return np.memmap(filename, dtype='bool_', mode='r', shape=tuple(size))
 
 
 def merge(uncompressed_graphs, sizes, outfile):
     ncols = sum([j for i, j in sizes])
-    nrows = sizes[0][0]
+    nrows = 10000  # sizes[0][0]
+    print(int(sizes[0][0]/nrows))
     _shape = (nrows, ncols)
     start = time.time()
-    ugs = []
-    for f, size in zip(uncompressed_graphs, sizes):
-        a = load_memmap(f, size)
-        ugs.append(a)
+
     with open(outfile, 'wb') as outf:
-        for i in range(nrows):
-            a = np.append([], [ugs[j][i, ] for j in range(len(ugs))])
-            if i % 10000 == 0:
-                logger.info(i)
-                logger.info(time.time()-start)
-                logger.info("%i MB" % int(process.memory_info().rss/1000000))
-            ba_out = bitarray(a.tolist())
-            outf.write(ba_out.tobytes())
+        for batch in range(int(sizes[0][0]/nrows)):
+            ugs = []
+            for f, size in zip(uncompressed_graphs, sizes):
+                a = load_memmap(f, 0 + (batch*nrows), nrows+(batch*nrows))
+                ugs.append(a)
+            for i in range(nrows):
+                a = np.append([], [ugs[j][i, ] for j in range(len(ugs))])
+                if (batch*nrows + i) % 10000 == 0:
+                    logger.info(batch*nrows + i)
+                    logger.info(time.time()-start)
+                    logger.info("%i MB" %
+                                int(process.memory_info().rss/1000000))
+                ba_out = bitarray(a.tolist())
+                outf.write(ba_out.tobytes())
     return {'graph': outfile, "shape": _shape}
