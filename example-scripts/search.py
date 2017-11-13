f35ca7bc070cb8d3647783f061aa4bd8e4df896b
Phelim Bradley <wave@phel.im>
2017-11-13 16:39:39 +0000

    Update examples script URL

--- example-scripts/search.py
+++ example-scripts/search.py
@@ -1,21 +1,20 @@
 import requests
 import begin
 
+
 def search(seq, threshold=1):
-    url="http://34.251.101.117:8081/search?threshold=%f&seq=%s" % (float(threshold),seq)
+    url = "http://api.cbgseq.com/search?threshold=%f&seq=%s" % (
+        float(threshold), seq)
     results = requests.get(url).json()
     samples = []
-    for i,j in list(results.values())[0]["results"].items():
+    for i, j in list(results.values())[0]["results"].items():
         samples.append(i)
     return samples
 
+
 @begin.start
 def main(seq, threshold=1):
-	samples = search(seq, threshold)
+    samples = search(seq, threshold)
 
-	for s in samples:
-		print(s)
-
-
-
-
+    for s in samples:
+        print(s)
