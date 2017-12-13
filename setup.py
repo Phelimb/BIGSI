5ef05a309ca761c377495cacc7d109a66420eb1b
Phelim Bradley <wave@phel.im>
2017-12-13 12:30:37 +0000

    revert setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 # from distutils.core import setup
 from setuptools import setup
 import os
-from pip.req import parse_requirements
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
-# required = ['cython', 'bsddb3', 'numpy', 'credis',
-#             'crc16', 'uwsgi', 'mmh3', 'HLL', 'bitarray', 'bitstring', 'redis',
-#             'hiredis', 'flask', 'biopython', 'celery', 'psutil']
-# dependency_links_list = ['git+git://github.com/Phelimb/atlas-var.git@9aee355b25d0f94b4603eb4bf7e8c47af793f5d4',
-#                          'git+git://github.com/Grokzen/redis-py-cluster@9f9baeced12b7abac53b3680ea3f5a1b16a38038',
-#                          'git+https://github.com/phelimb/pyseqfile',
-#                          "git+git://github.com/timothycrosley/hug.git@e6e85e4e4332fba6d01273b3719c1c11abc644e0"]
-
-install_reqs = parse_requirements('requirements.txt', session=False)
-required = [str(ir.req) for ir in install_reqs if ir]
-print(required)
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 from Cython.Build import cythonize
 
 setup(
@@ -39,7 +27,7 @@ setup(
     description='BItsliced Genomic Signature Index - Efficient indexing and search in very large collections of WGS data',
     author='Phelim Bradley',
     author_email='wave@phel.im',
-    install_requires=required,
+    install_requires=required[4:],
     entry_points={
         'console_scripts': [
             'bigsi = bigsi.__main__:main',
